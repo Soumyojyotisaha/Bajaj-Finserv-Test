e4dcf8bf072d2f24135a8a9cdaef6c223ae78865
Soumyojyotisaha <soumyojyotisaha2001offic@gmail.com>
2024-08-25 11:34:14 +0530

    code update

--- netlify/functions/bfh1.js
+++ netlify/functions/bfh1.js
@@ -1,27 +1,25 @@
 const express = require('express');
-const serverless = require('serverless-http');
-
 const app = express();
+
 app.use(express.json());
 
-// POST endpoint
+// POST /bfhl route
 app.post('/bfhl', (req, res) => {
     const { data } = req.body;
     const user_id = "soumyojyoti_saha_29122001";
     const email = "soumyojyotisaha2001offic@gmail.com";
     const roll_number = "21BCE4007";
-
     let numbers = [];
     let alphabets = [];
-    let highest_lowercase = '';
+    let highestLowercase = "";
 
     data.forEach(item => {
         if (!isNaN(item)) {
             numbers.push(item);
-        } else if (/[a-zA-Z]/.test(item)) {
+        } else if (typeof item === 'string') {
             alphabets.push(item);
-            if (/[a-z]/.test(item) && item > highest_lowercase) {
-                highest_lowercase = item;
+            if (item >= 'a' && item <= 'z' && item > highestLowercase) {
+                highestLowercase = item;
             }
         }
     });
@@ -33,16 +31,18 @@ app.post('/bfhl', (req, res) => {
         roll_number: roll_number,
         numbers: numbers,
         alphabets: alphabets,
-        highest_lowercase_alphabet: highest_lowercase ? [highest_lowercase] : []
+        highest_lowercase_alphabet: highestLowercase ? [highestLowercase] : []
     });
 });
 
-// GET endpoint
+// GET /bfhl route
 app.get('/bfhl', (req, res) => {
     res.status(200).json({
         operation_code: 1
     });
 });
 
-// Export the serverless function
-module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
